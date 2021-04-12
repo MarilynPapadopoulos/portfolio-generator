e744ca084abfdd02726d093c426b42e0aecf1908
MarilynPapadopoulos <marilyn.volk@gmail.com>
2021-04-12 14:23:40 -0400

    created data array function

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
-var profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+
+const printProfileData = profileDataArr => {
+    for(let i = 0; i< profileDataArr.length; i+=1){
+        console.log(profileDataArr[i]);
+    }
+   console.log('=============');
+
+   profileDataArr.forEach(profileItem => console.log(profileItem));
+};
+
+printProfileData(profileDataArgs);
